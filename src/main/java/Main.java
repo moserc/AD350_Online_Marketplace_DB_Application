55e0301e1ba2cb0e9564c11778884adc9c2ea04a
Riberiko <67806247+Riberiko@users.noreply.github.com>
2023-06-11 10:49:30 -0700

    inital start added drivers to gradle file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,71 +7,75 @@ public class Main {
 
         Connection conn = null;
 
+        //Capture username & password
+        Scanner input = new Scanner(System.in);
+        String url = "jdbc:mysql://localhost:3306/ACME";
+
+        System.out.print("mysql username: ");
+        String username = input.nextLine();
+
+        System.out.print("password: ");
+        String password = input.nextLine();
+
         try{
-            //Capture username & password
-            Scanner input = new Scanner(System.in);
-            String url = "jdbc:mysql://localhost:3306/ACME";
-
-            System.out.println("mysql username: ");
-            String username = input.nextLine();
-
-            System.out.println("password: ");
-            String password = input.nextLine();
-
             //Connect
             conn = DriverManager.getConnection(url, username, password); //connector jar file must be in the classpath
             System.out.println("Connection successful.");
-
-            //do stuff
-            int option;
-            while (true) {
-                System.out.println("Menu options: ");
-                System.out.println("1 --> <placeholder>");
-                System.out.println("2 --> <placeholder>");
-                System.out.println("3 --> <placeholder>");
-                System.out.println("4 --> <placeholder>");
-                System.out.println("5 --> <placeholder>");
-                System.out.println("6 --> <placeholder>");
-                System.out.println("7 --> <placeholder>");
-                System.out.println("8 --> exit");
-
-                option = input.nextInt();
-
-                switch (option) {
-                    case 1:
-                        System.out.println("placeholder");
-                        break;
-                    case 2:
-                        System.out.println("placeholder");
-                        break;
-                    case 3:
-                        System.out.println("placeholder");
-                        break;
-                    case 4:
-                        System.out.println("placeholder");
-                        break;
-                    case 5:
-                        System.out.println("placeholder");
-                        break;
-                    case 6:
-                        System.out.println("placeholder");
-                        break;
-                    case 7:
-                        System.out.println("placeholder");
-                        break;
-                    case 8:
-                        //close connection
-                        conn.close();
-                        System.out.println("Connection closed. Goodbye.");
-                        System.exit(0);
-                        break;
-                    default:
-                        System.out.println("Menu options are integers 1 - 8.");
-                        break;
-                }
-            }
-        }catch (Exception e){
-            System.out.println("Cannot connect to DB: " + e);
+        }catch (SQLException e){
+            System.out.println("Cannot connect to DB: " + e.getMessage());
+            System.exit(0);
         }
+
+        //do stuff
+        int option = -1;
+        while (option != 8) {
+            System.out.println("Menu options: ");
+            System.out.println("1 --> <placeholder>");
+            System.out.println("2 --> <placeholder>");
+            System.out.println("3 --> Create new Product");
+            System.out.println("4 --> Least Popular Items in time range");
+            System.out.println("5 --> <placeholder>");
+            System.out.println("6 --> <placeholder>");
+            System.out.println("7 --> <placeholder>");
+            System.out.println("8 --> exit");
+
+            System.out.print("Enter Option: ");
+            option = input.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("placeholder");
+                    break;
+                case 2:
+                    System.out.println("placeholder");
+                    break;
+                case 3:
+                    System.out.println("Create New Product");
+                    break;
+                case 4:
+                    System.out.println("Least Popular Items in time range");
+                    break;
+                case 5:
+                    System.out.println("placeholder");
+                    break;
+                case 6:
+                    System.out.println("placeholder");
+                    break;
+                case 7:
+                    System.out.println("placeholder");
+                    break;
+                default:
+                    System.out.println("Menu options are integers 1 - 8.");
+                    break;
+            }
+        }
+
+        input.close();
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Connection closed. Goodbye.");
     }
 }
